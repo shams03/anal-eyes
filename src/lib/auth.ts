3b6d2f494b5c79da5ceea4df551c4d3b11bd59ec
shams03 <95904933+shams03@users.noreply.github.com>
2025-05-18 01:18:24 +0530

    production next_auth fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/db";
 
 // Extend the Session type to include id
@@ -17,8 +17,47 @@ declare module "next-auth" {
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
   session: {
     strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        const dbUser = await prisma.user.findFirst({
+          where: { email: user.email as string },
+        });
+
+        if (!dbUser) {
+          token.id = user.id;
+          token.email = user.email;
+          token.name = user.name;
+          token.picture = user.image;
+        } else {
+          token.id = dbUser.id;
+          token.email = dbUser.email;
+          token.name = dbUser.name;
+          token.picture = dbUser.image;
+        }
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
   },
   cookies: {
     sessionToken: {
@@ -29,58 +68,6 @@ export const authOptions: NextAuthOptions = {
         path: "/",
         secure: process.env.NODE_ENV === "production",
       },
-    },  },
-  pages: {
-    signIn: "/auth/signin",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        session.user.id = token.id as string;
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-      }
-      console.log("token", token);
-
-      return session;
-    },
-    async jwt({ token, user }) {
-      console.log("JWT Callback:", { token, user });
-      // Check if it's the first sign-in (when `user` is available)
-      if (user) {
-        // Find the user in the database based on their email
-        const dbUser = await prisma.user.findFirst({
-          where: {
-            email: user.email as string,
-          },
-        });
-        console.log("dbUser", dbUser);
-        console.log("user", user);
-
-        // If the user doesn't exist in the database, create one or handle accordingly
-        if (!dbUser) {
-          // Here, if the user doesn't exist, you can assign data to `token` directly
-          token.id = user.id; // You can map this to your database model
-          token.email = user.email;
-          token.name = user.name;
-          token.picture = user.image;
-        } else {
-          // If the user exists, make sure to load data into the token
-          token.id = dbUser.id;
-          token.email = dbUser.email;
-          token.name = dbUser.name;
-          token.picture = dbUser.image;
-        }
-      }
-
-      return token;
     },
   },
   debug: true,
